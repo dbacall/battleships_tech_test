326f91de1de88ddc1f28d9697e92809955f2636a
dbacall <dbacall@hotmail.co.uk>
2020-01-30 12:15:26 +0000

    test: add test for checking if ship sunk

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,9 +1,9 @@
-require_relative '../../lib/ship.rb'
+require_relative '../lib/ship.rb'
 
 describe Ship do
 
   describe '#sunk?' do
-    it "returns false if ship not sunk" do
+    it "returns false" do
       ship = Ship.new([[1, 2], [2, 2]])
       expect(ship.sunk?).to eq false
     end
