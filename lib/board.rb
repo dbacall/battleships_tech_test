54b1c93887f9665aae00e030c6c8e9a791aad05f
dbacall <dbacall@hotmail.co.uk>
2020-01-30 13:40:55 +0000

    feature: add ability to mark a hit on a board

--- lib/board.rb
+++ lib/board.rb
@@ -11,6 +11,12 @@ class Board
     @layout[row][column] = 'm'
   end
 
+  def mark_hit(coordinate)
+    row = coordinate[0] - 1
+    column = coordinate[1] - 1
+    @layout[row][column] = 'h'
+  end
+
   def show
     puts "  1 2 3 4 5 6 7 8 9"
     @layout.each.with_index(1) { |row, index|
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -38,4 +38,22 @@ describe Board do
       ).to_stdout
     end
   end
+
+  describe '#mark_board' do 
+    it 'marks hits on the board' do
+      board.mark_hit([2, 3])
+      expect { board.show }.to output(
+      "  1 2 3 4 5 6 7 8 9\n"\
+      "1 x x x x x x x x x\n"\
+      "2 x x h x x x x x x\n"\
+      "3 x x x x x x x x x\n"\
+      "4 x x x x x x x x x\n"\
+      "5 x x x x x x x x x\n"\
+      "6 x x x x x x x x x\n"\
+      "7 x x x x x x x x x\n"\
+      "8 x x x x x x x x x\n"\
+      "9 x x x x x x x x x\n"
+      ).to_stdout
+    end
+  end
 end
