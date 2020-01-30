893adce6999a1ac56004d9e97668612433f08220
dbacall <dbacall@hotmail.co.uk>
2020-01-30 10:25:53 +0000

    feature: can add more than one ship

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,9 @@
 class Game
 
+  def initialize
+    @ships = []
+  end
+
   def add_ship(coordinate)
     ship = [coordinate]
     i = 1
@@ -7,7 +11,7 @@ class Game
       ship << [coordinate[0] + i, coordinate[1]]
       i += 1
     }
-    ship
+    @ships << ship
   end
 
 end
