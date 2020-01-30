8c88de1958b6e38fdefc8f4fdf2c1e8c85f23f2d
dbacall <dbacall@hotmail.co.uk>
2020-01-30 10:46:26 +0000

    refactor: create private methods for left and right coordinates

--- lib/game.rb
+++ lib/game.rb
@@ -4,14 +4,31 @@ class Game
     @ships = []
   end
 
-  def add_ship(length = 3, coordinate)
-    ship = [coordinate]
-    i = 1
+  def add_ship(length = 3, coordinate, direction)
+    @ship = [coordinate]
+    @index = 1
+    if direction == 'left'
+      left_coordinates(length, coordinate)
+    else
+      right_coordinates(length, coordinate)
+    end
+    @ships << @ship
+  end
+
+  private
+
+  def left_coordinates(length, coordinate)
     (length-1).times { 
-      ship << [coordinate[0] + i, coordinate[1]]
-      i += 1
+      @ship << [coordinate[0] - @index, coordinate[1]]
+      @index += 1
     }
-    @ships << ship
+  end
+
+  def right_coordinates(length, coordinate)
+    (length-1).times { 
+      @ship << [coordinate[0] + @index, coordinate[1]]
+      @index += 1
+    }
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,17 +6,21 @@ describe Game do
 
   describe '#add_ship' do
     it 'adds a ship starting at a specific coordinate' do
-      expect(game.add_ship([2, 3])).to eq [[[2, 3], [3, 3], [4, 3]]]
+      expect(game.add_ship([2, 3], 'right')).to eq [[[2, 3], [3, 3], [4, 3]]]
     end
 
     it 'can add more than one ship' do
-      game.add_ship([2, 3])
-      expect(game.add_ship([3, 6])).to eq [[[2, 3], [3, 3], [4, 3]], 
+      game.add_ship([2, 3], 'right')
+      expect(game.add_ship([3, 6], 'right')).to eq [[[2, 3], [3, 3], [4, 3]], 
       [[3, 6], [4, 6], [5, 6]]]
     end
 
     it 'can add ships with different sizes' do
-      expect(game.add_ship(5, [2, 3])).to eq [[[2, 3], [3, 3], [4, 3], [5, 3], [6, 3]]]
+      expect(game.add_ship(5, [2, 3], 'right')).to eq [[[2, 3], [3, 3], [4, 3], [5, 3], [6, 3]]]
+    end
+
+    it 'can choose the direction of the ship to be left' do
+      expect(game.add_ship(3, [7, 3], 'left')).to eq [[[7, 3], [6, 3], [5, 3]]]
     end
   end
 
